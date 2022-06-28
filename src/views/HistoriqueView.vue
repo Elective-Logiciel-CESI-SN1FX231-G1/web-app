e83f49baab9d8eabd55c93fabd77c1a12ca37e2e
OlivierS7 <olivier.simonneau@viacesi.fr>
2022-06-28 15:16:57 +0200

    Client History

--- src/views/HistoriqueView.vue
+++ src/views/HistoriqueView.vue
@@ -6,16 +6,16 @@
           <v-row>
             <v-col class="col-12 text-center">
               <h2>Historique des livraisons effectuées</h2>
-              <v-alert type="warning" text v-if="deliveredOrders.length === 0">
+              <v-alert type="warning" text v-if="orders.length === 0">
                 Vous n'avez pas encore fait de commande
               </v-alert>
               <v-card outlined v-else>
                 <v-data-iterator
-                  :items="deliveredOrders"
+                  :items="orders"
                   @pagination="fetchData"
-                  :page.sync="deliveredOrdersPage"
-                  :items-per-page.sync="deliveredOrdersPerPage"
-                  :server-items-length="deliveredOrdersCount"
+                  :page.sync="ordersPage"
+                  :items-per-page.sync="ordersPerPage"
+                  :server-items-length="ordersCount"
                 >
                   <template v-slot:default="props">
                     <v-list>
@@ -29,7 +29,7 @@
                                 </v-col>
                                 <v-spacer></v-spacer>
                                 <v-col style="flex-grow: 0;">
-                                  {{ order.deliveryPrice }}€
+                                  {{ order.price }}€
                                 </v-col>
                               </v-row>
                             </v-list-item-title>
@@ -78,36 +78,104 @@
         </v-card>
       </v-container>
     </div>
+    <div v-if="user && user.role === 'client'">
+      <v-container>
+        <v-card outlined>
+          <v-row>
+            <v-col class="col-12 text-center">
+              <h2>Historique des commandes effectuées</h2>
+              <v-alert type="warning" text v-if="orders.length === 0">
+                Vous n'avez pas encore fait de commande
+              </v-alert>
+              <v-card outlined v-else>
+                <v-data-iterator
+                  :items="orders"
+                  @pagination="fetchData"
+                  :page.sync="ordersPage"
+                  :items-per-page.sync="ordersPerPage"
+                  :server-items-length="ordersCount"
+                >
+                  <template v-slot:default="props">
+                    <v-list>
+                      <template v-for="order in props.items">
+                        <v-list-item :key="order._id">
+                          <v-list-item-content class="mr-4" >
+                            <v-list-item-title>
+                              <v-row>
+                                <v-col>
+                                  <p>{{ order.restaurant.name }}, {{ order.restaurant.address }}</p>
+                                </v-col>
+                                <v-col>
+                                  <p>{{ order.deliveringDate }}</p>
+                                </v-col>
+                                <v-spacer></v-spacer>
+                                <v-col style="flex-grow: 0;">
+                                  {{ order.price }}€ + {{ order.deliveryPrice + order.commissionPrice }}€ (Frais)
+                                </v-col>
+                              </v-row>
+                            </v-list-item-title>
+                          </v-list-item-content>
+                          <v-dialog
+                            transition="dialog-transition"
+                          >
+                            <template v-slot:activator="{ on, attrs }">
+                              <v-btn text fab small v-bind="attrs" v-on="on">
+                                <v-icon>mdi-eye</v-icon>
+                              </v-btn>
+                            </template>
+                            <template v-slot:default="dialog">
+                              <v-card>
+                                <Commande :value="order"/>
+                                <v-card-actions>
+                                  <v-spacer></v-spacer>
+                                  <v-btn color="secondary" @click="dialog.value=false">Fermer la fenêtre</v-btn>
+                                </v-card-actions>
+                              </v-card>
+                            </template>
+                          </v-dialog>
+                        </v-list-item>
+                        <v-divider  :key="'d'+order._id"></v-divider>
+                      </template>
+                    </v-list>
+                  </template>
+                </v-data-iterator>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-container>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import { mapState } from 'vuex'
+import Commande from '../components/Commande.vue'
 
 export default Vue.extend({
   name: 'Historique',
   data: () => ({
-    deliveredOrders: [],
-    deliveredOrdersCount: 0,
-    deliveredOrdersPage: 1,
-    deliveredOrdersPerPage: 15
+    orders: [],
+    ordersCount: 0,
+    ordersPage: 1,
+    ordersPerPage: 15
   }),
   components: {
-
+    Commande
   },
   computed: {
     ...mapState('auth', ['user'])
   },
   methods: {
     async fetchData () {
-      const delivOrders = (await this.axios.get('/orders/api/orders?status=completed&deliverer=me')).data
-      this.deliveredOrders = delivOrders.results
-      this.deliveredOrdersCount = delivOrders.count
+      const OrdersResult = (await this.axios.get('/orders/api/orders?status=completed')).data
+      this.orders = OrdersResult.results
+      this.ordersCount = OrdersResult.count
     }
   },
   async mounted () {
-    this.deliveredOrders = (await this.axios.get('/orders/api/orders?status=completed&deliverer=me')).data.results
+    await this.fetchData()
   }
 })
 </script>
